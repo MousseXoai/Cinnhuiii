2dc17b88e28c8ea4456f52b74e690ecbb0b03d63
KeoBongg <nguyentrongquan27112003@gmail.com>
2023-09-15 01:56:54 +0700

    init project

---
+++ CONFIG.js
@@ -0,0 +1,8 @@
+const CONFIGDATA = {
+    titleHeader: "Helu, Anh có điều muốn nói :<. </br> Cho anh cin nhũi vì đã làm em buồn nha :<",
+    descriptionHeader: "Bạn nhỏ tha lỗi cho anh nha :< Lần sau anh sẽ hông như vậy nữa đâu huhu :<",
+    buttonYes: "Em đồng ý ạaaaa",
+    buttonNo: "Không đấy :))",
+    titleModar: "Cảm ơn bạn nhỏ gấc nhìu nhaaaaaa ",
+    descriptionModar: "Cảm ơn bạn nhỏ đã cho anh một cơ hội để tớ có thể sửa chữa sai lầm :<, có thể ở cạnh bạn nhỏ </br> Anh biết anh hông đẹp trai, cũng chẳng hoàn hảo nhưng anh có một trái tim chân thành. Anh sợ bạn nhỏ dỗi lắm huhu :< </br> Hông biết đêm nay bạn nhỏ ngụ được hông, nhma cố gắng ngụ ngon, đừng buồn nhé >< </br>Sáng mai sẽ có một bất ngờ dành cho bạn nhỏ ó hjhj. Bạn nhỏ tha lũi cho anh nhaa <3",
+};
